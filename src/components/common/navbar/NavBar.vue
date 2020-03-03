4c17e9135116b4f1e4a4e50c7eb21656bb15f286
1937871222gh <1937871222@qq.com>
2020-03-03 22:44:11 +0800

    '1'

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="nav-bar">
+    <div class="nav-left"><slot name="left"></slot></div>
+    <div class="nav-center"><slot name="center">标题</slot></div>
+    <div class="nav-right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+  #nav-bar{
+    display: flex;
+    position: relative;
+    z-index: 10;
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+  }
+.nav-left,.nav-right{
+  width: 60px;
+}
+  .nav-center{
+    flex: 1;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,141 @@
 <template>
     <div id="home">
-        home
+        <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
+        <home-swiper :banners="banners"/>
+        <recommend-view :recommends="recommends"/>
+        <feature-view/>
+        <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick"/>
+        <goods-list :goods="showGoods"/>
+        <ul>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+            <li>我擦</li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import NavBar from "components/common/navbar/NavBar";
+    import HomeSwiper from "./childComps/HomeSwiper";
+    import RecommendView from "./childComps/RecommendView";
+    import FeatureView from "./childComps/FeatureView";
+    import TabControl from "components/content/tabControl/TabControl";
+    import GoodsList from "components/content/goods/GoodsList";
+    import GoodsListItem from "components/content/goods/GoodsListItem";
+
+    import {getHomeMultidata,getHomeGoods} from "network/home";
+
+
     export default {
-        name: "Home"
+        name: "Home",
+        data(){
+            return{
+                banners:[],
+                recommends:[],
+                goods: {
+                    'pop':{page: 0, list: []},
+                    'new':{page: 0, list: []},
+                    'sell':{page: 0, list: []}
+                },
+                currentType:'pop'
+            }
+        },
+        components: {
+            NavBar,
+            HomeSwiper,
+            RecommendView,
+            FeatureView,
+            TabControl,
+            GoodsList,
+            GoodsListItem
+        },
+        created() {
+            // 1.请求多个数据
+            this.getHomeMultidata()
+
+            //    2.请求商品数据
+            this.getHomeGoods('pop')
+
+            this.getHomeGoods('new')
+
+            this.getHomeGoods('sell')
+
+        },
+        computed:{
+            showGoods(){
+                return this.goods && this.goods[this.currentType].list
+            }
+        },
+        methods: {
+
+            // 事件监听相关
+            tabClick(index){
+                switch (index) {
+                    case 0:
+                        this.currentType = 'pop'
+                        break
+                    case 1:
+                        this.currentType = 'new'
+                        break
+                    case 2:
+                        this.currentType = 'sell'
+                }
+            },
+            // 事件监听相关
+
+            // 网络请求相关
+            getHomeMultidata(){
+                getHomeMultidata().then(res => {
+                    this.banners = res.data.banner.list;
+                    this.recommends =res.data.recommend.list;
+                })
+            },
+            getHomeGoods(type) {
+                // 值得思考
+                // 只能goods[type]这样调用，不能goods.type，因为传过来的是'type'
+                const page =this.goods[type].page + 1
+                getHomeGoods(type,page).then(res => {
+                    // console.log(res);
+                    this.goods[type].list.push(...res.data.list)
+                    //    ES6语法将请求数据list push到data的对应goods里面
+                    this.goods[type].page += 1
+                    // 上拉完成调用这个方法才能再次上拉
+                    // this.$refs.scroll.finishPullUp()
+                })
+            }
+            // 网络请求相关
+        }
     }
 </script>
 
 <style scoped>
-
+  #home{
+    padding-top: 44px;
+  }
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+    font-weight: 700;
+    position: fixed !important;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
 </style>
