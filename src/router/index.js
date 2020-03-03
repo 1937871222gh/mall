fa09ca62e8ec7ba9fac005f2643c66bd42db040e
1937871222gh <1937871222@qq.com>
2020-03-03 14:08:56 +0800

    '3'

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,33 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Cart = () => import('views/cart/Cart')
+const Profile = () => import('views/profile/Profile')
+// const Detail = () => import('views/detail/Detail')
 
+const routes = [
+  {
+    path:'',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
 ]
 
 const router = new VueRouter({
