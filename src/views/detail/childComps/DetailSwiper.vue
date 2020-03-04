079ca2ff68d29fe1fd9c78f3ae299d3956cbe8ee
1937871222gh <1937871222@qq.com>
2020-03-04 20:14:47 +0800

    '1'

---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+    <swiper>
+        <swiper-item class="detail-swiper" v-for="(item,index) in topImages" v-if="topImages.length" :key="index">
+            <img :src="item" alt="">
+        </swiper-item>
+    </swiper>
+</template>
+
+<script>
+    import {Swiper, SwiperItem} from 'components/common/swiper'
+    export default {
+        name: "DetailSwiper",
+        props:{
+            topImages: {
+                type:Array,
+                default(){
+                    return []
+                }
+            }
+        },
+        components:{
+            Swiper,
+            SwiperItem
+        }
+    }
+</script>
+
+<style scoped>
+    .detail-swiper{
+        height: 300px;
+        overflow: hidden;
+    }
+</style>
