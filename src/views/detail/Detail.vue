60c6703d087559e4a85d1951fbede8b26f88267d
1937871222gh <1937871222@qq.com>
2020-03-04 22:21:59 +0800

    '1'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,6 +3,10 @@
         <detail-nav-bar/>
         <detail-swiper :top-images="topImages"/>
         <detail-base-info :goods="goods"/>
+        <detail-shop-info :shop="shop"/>
+        <detail-goods-info :detail-info="detailInfo"/>
+        <detail-param-info :param-info="paramInfo"/>
+        <detail-comment-info :comment-info="commentInfo"/>
     </div>
 </template>
 
@@ -10,8 +14,12 @@
     import DetailNavBar from "./childComps/DetailNavBar";
     import DetailSwiper from "./childComps/DetailSwiper";
     import DetailBaseInfo from "./childComps/DetailBaseInfo";
+    import DetailShopInfo from "./childComps/DetailShopInfo";
+    import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+    import DetailParamInfo from "./childComps/DetailParamInfo";
+    import DetailCommentInfo from "./childComps/DetailCommentInfo";
 
-    import {getDetail,Goods} from "network/detail";
+    import {getDetail,Goods,Shop,GoodsParam} from "network/detail";
 
     export default {
         name: "Detail",
@@ -19,13 +27,21 @@
             return{
                 iid:null,
                 topImages: [],
-                goods:{}
+                goods:{},
+                shop: {},
+                detailInfo:{},
+                paramInfo: {},
+                commentInfo:{},
             }
         },
         components: {
             DetailNavBar,
             DetailSwiper,
-            DetailBaseInfo
+            DetailBaseInfo,
+            DetailShopInfo,
+            DetailGoodsInfo,
+            DetailParamInfo,
+            DetailCommentInfo
         },
         created() {
             // 1.保存传入的iid
@@ -39,6 +55,20 @@
 
                 //    2.获取商品信息,把数据整合为一个对象
                 this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+
+                //   3.创建店铺信息的对象
+                this.shop =new Shop(data.shopInfo)
+
+                // 4.获取商品详情信息
+                this.detailInfo = data.detailInfo
+
+                // 5.保存参数信息
+                this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule);
+
+                // 6.保存评论信息
+                if (data.rate.list) {
+                    this.commentInfo = data.rate.list[0];
+                }
             })
         }
     }
